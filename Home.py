926cd2f8af699855202b7e488fa70ee5ee7b415a
Béla J. Szekeres <75852587+szbela87@users.noreply.github.com>
2025-05-25 18:02:17 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -3,7 +3,7 @@ warnings.filterwarnings("ignore", category=FutureWarning)
 import streamlit as st
 
 st.set_page_config(layout="wide")
-st.title("🏠 Welcome to the Vendor Analysis App! +++")
+st.title("🏠 Welcome to the Vendor Analysis App!")
 
 st.markdown("""
 This application is a PandasAI + OpenAI API + Streamlit demo and offers three main features:
@@ -11,4 +11,10 @@ This application is a PandasAI + OpenAI API + Streamlit demo and offers three ma
 - **ChatCSV**: Chat with the CSV file
 - **Preset Reports**: Predefined reports and statistics
 - **Vendor Feedback Analysis**: Process vendor feedback from a separate CSV file
+""")
+
+st.markdown("""
+---
+
+📂 **Source code available on [GitHub](https://github.com/szbela87/streamlit_customer_service)**.
 """)
